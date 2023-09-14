9159486655b1f79418bcbea70bce699f196613e6
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-09-14 12:26:14 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MovieList from './components/MovieList';
 import MovieDetails from './components/MovieDetails';
+import Sidebar from './components/SideBar';
 import './App.css'
 
 function App() {
   return (
     <Router>
+      <div className="app-container">
+        <Sidebar />
+      </div>
+      <div className="main-content">
       <Routes>
     
         <Route path="/" element={<MovieList />} />
         <Route path="/movies/:id" element={<MovieDetails />} />
         
-      </Routes>
+        </Routes>
+      </div>
     </Router>
   );
 }
