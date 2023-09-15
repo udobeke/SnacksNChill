8e7ef26361dc13c9c2689eb468906162de326b01
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-09-15 13:17:32 +0100

    Update MovieDetails.jsx

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,42 +1,43 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Sidebar from './SideBar';
-import axios from 'axios';
-import '../App.css'
-
-const API_KEY = '2379ba0eff6a55ce45794c90a648db8b';
+import { Link } from 'react-router-dom';
 
 function MovieDetails() {
   const { id } = useParams();
   const [movie, setMovie] = useState(null);
 
   useEffect(() => {
-    axios
-      .get(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`)
-      .then((response) => {
-        setMovie(response.data);
-      })
-      .catch((error) => {
-        console.error('Error fetching movie details:', error);
-      });
+    // Fetch movie details by ID from the TMDB API
+    const apiKey = '2379ba0eff6a55ce45794c90a648db8b';
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`)
+      .then((response) => response.json())
+      .then((data) => setMovie(data))
+      .catch((error) => console.error('Error fetching movie details:', error));
   }, [id]);
 
   if (!movie) {
-    return <div>Loading...</div>;
+    return <p>Loading...</p>;
   }
 
   return (
     <>
-    <Sidebar />
-    <div>
-      <h1>{movie.title}</h1>
-      <img
-        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-        alt={movie.title}
-      />
-      <p>{movie.release_date}</p>
-      <p>{movie.runtime} minutes</p>
-      <p>{movie.overview}</p>
+      <div className='description'>
+        <h2 className="logo">
+          <Link to="/">SnacksNChill</Link>
+        </h2>
+      </div>
+      <div className="movie-details">
+        <img
+          src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+          alt={movie.title}
+          className="movie-poster"
+        />
+        <div className='font'>
+          <h2  data-testid="movie-title">{movie.title}</h2>
+          <p className='font1' data-testid="movie-release-date">Release Date (UTC): {movie.release_date}</p>
+          <p data-testid="movie-runtime">Runtime (minutes): {movie.runtime}</p>
+          <p data-testid="movie-overview">{movie.overview}</p>
+        </div>
       </div>
     </>
   );
